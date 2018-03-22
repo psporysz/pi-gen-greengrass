f2ccfbdd4c24866131e122c8004185a5b60260d2
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-03-22 03:42:49 +0000

    Delete empty config & certs dirs for greengrass
    These directories are empty by default or contain
    non-working examples. We want to import working
    configurations from users instead.
    
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- stage99/01-setup-config/00-run.sh
+++ stage99/01-setup-config/00-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Users should place the extracted keys/config into the fat32 partition.
+rm -rf ${ROOTFS_DIR}/greengrass/certs \
+       ${ROOTFS_DIR}/greengrass/config
 ln -s /boot/certs/ ${ROOTFS_DIR}/greengrass/certs
 ln -s /boot/config/ ${ROOTFS_DIR}/greengrass/config
