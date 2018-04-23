343a706e1ef8dd3444abf7b5a85a59deb24f06fa
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-04-23 23:14:45 +0000

    Sqlite and protected links
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- stage99/00-install-greengrass/00-run.sh
+++ stage99/00-install-greengrass/00-run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+cat <<EOF >>/etc/sysctl.d/98-rpi.conf
+fs.protected_hardlinks = 1
+fs.protected_symlinks = 1
+EOF
+
+apt-get install sqlite3
+
 tar Czxf ${ROOTFS_DIR} files/greengrass*.tar.gz
