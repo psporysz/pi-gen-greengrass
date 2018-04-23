fc541d5ce259954a623d0145b63405754ab35bef
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-04-23 15:40:05 +0000

    Framebuffer size & fix ln
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- stage99/01-setup-config/00-run.sh
+++ stage99/01-setup-config/00-run.sh
@@ -2,5 +2,11 @@
 # Users should place the extracted keys/config into the fat32 partition.
 rm -rf ${ROOTFS_DIR}/greengrass/certs \
        ${ROOTFS_DIR}/greengrass/config
-ln -s /boot/certs/ ${ROOTFS_DIR}/greengrass/certs
-ln -s /boot/config/ ${ROOTFS_DIR}/greengrass/config
+ln -s ${ROOTFS_DIR}/greengrass/certs /boot/greengrass/certs
+ln -s ${ROOTFS_DIR}/greengrass/config /boot/greengrass/config
+
+# For twitch stream we want viewers to see our text BIG!
+cat <<EOF >>/boot/config.txt
+framebuffer_width=800
+framebuffer_height=400
+EOF
