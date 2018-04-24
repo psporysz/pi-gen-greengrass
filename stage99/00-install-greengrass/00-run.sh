8c6e871ac123ecca17eea317942b37dd8bb08772
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-04-24 02:03:46 +0000

    Fix chroot of stage99
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- stage99/00-install-greengrass/00-run.sh
+++ stage99/00-install-greengrass/00-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cat <<EOF >>/etc/sysctl.d/98-rpi.conf
+cat <<EOF >>${ROOTFS_DIR}/etc/sysctl.d/98-rpi.conf
 fs.protected_hardlinks = 1
 fs.protected_symlinks = 1
 EOF
 
-apt-get install sqlite3
-
 tar Czxf ${ROOTFS_DIR} files/greengrass*.tar.gz
+
+git clone git://github.com/aws-samples/aws-greengrass-samples ${ROOTFS_DIR}/home/pi/aws-greengrass-samples
